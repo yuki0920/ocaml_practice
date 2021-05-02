4142b0fce0d860292bd1e882df6116f9120cca17
yuki0920 <ywsep20@gmail.com>
2021-05-03 07:42:03 +0900

    Fix insert.ml

--- insert.ml
+++ insert.ml
@@ -1,10 +1,10 @@
 let rec insert lst n = match lst with
-  [] -> []
+  [] -> [n]
   | first :: rest ->
-    if n >= first
-    then insert n::first::rest n
-    else insert rest n
+    if first < n
+    then first :: insert rest n
+    else n :: lst
 
 let test1 = insert [] 1 = [1]
-let test2 = insert [1, 3] 2 = [1, 2, 3]
-let test3 = insert [1, 3, 4, 7, 8] 5 = [1, 3, 4, 5, 7, 8]
+let test2 = insert [1; 3] 2 = [1; 2; 3]
+let test3 = insert [1; 3; 4; 7; 8] 5 = [1; 3; 4; 5; 7; 8]
