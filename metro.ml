4dab6991c898e72121dd02a154279aad1b4c9ded
yuki0920 <ywsep20@gmail.com>
2021-05-02 10:29:37 +0900

    metro.ml

---
+++ metro.ml
@@ -0,0 +1,12 @@
+type ekimei_t = {
+  kanji : string;
+  kana : string;
+  romaji : string;
+  shozoku : string;
+}
+
+let hyouji ekimei = match ekimei with
+  {kanji = kanji; kana = kana; romaji = romaji; shozoku = shozoku} ->
+    shozoku ^ ", " ^ kanji ^ "（" ^ kana ^ "）"
+
+let hyouji_test = hyouji {kanji = "茗荷谷"; kana = "みょうがだに"; romaji = "myougadani"; shozoku = "丸ノ内線"} = "丸ノ内線, 茗荷谷（みょうがだに）"
