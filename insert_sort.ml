c5268f5867f41e23df3488156e0f2cbd49f2173f
yuki0920 <ywsep20@gmail.com>
2021-05-03 08:16:38 +0900

    Fix insert_sort.ml

--- insert_sort.ml
+++ insert_sort.ml
@@ -1,8 +1,15 @@
 (* 目的: リストを受け取り並び替えたリストを返す *)
 (* int list -> int -> int list *)
+let rec insert lst n = match lst with
+  [] -> [n]
+  | first :: rest ->
+    if first < n
+    then first :: insert rest n
+    else n :: lst
+
 let rec insert_sort lst = match lst with
   [] -> []
-  | first :: rest -> first::rest
+  | first :: rest -> insert (insert_sort rest) first
 
 let test1 = insert_sort [] = []
 let test2 = insert_sort [1] = [1]
