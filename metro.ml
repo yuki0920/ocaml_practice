aadc655dcf73edfcc8b9c64636598058e2fa4ae4
yuki0920 <ywsep20@gmail.com>
2021-05-04 11:08:31 +0900

    Add make_initial_eki_list

--- metro.ml
+++ metro.ml
@@ -410,6 +410,31 @@ let test_shokika1 = shokika [{namae="代々木上原"; saitan_kyori = infinity; 
 let test_shokika2 = shokika [{namae="代々木上原"; saitan_kyori = infinity; temae_list = []}; {namae="代々木公園"; saitan_kyori = infinity; temae_list = []}] "代々木上原"
   = [{namae="代々木上原"; saitan_kyori = 0.; temae_list = ["代々木上原"]};  {namae="代々木公園"; saitan_kyori = infinity; temae_list = []}]
 
+let make_initial_eki_list ekimei_t_list kiten = List.map(
+  fun ekimei_t ->
+    if ekimei_t.kanji = kiten then {namae = ekimei_t.kanji; saitan_kyori = 0.; temae_list = [kiten]}
+    else {namae = ekimei_t.kanji; saitan_kyori = infinity; temae_list = []}
+  ) ekimei_t_list
+
+let ekimei_list = [
+{kanji="池袋"; kana="いけぶくろ"; romaji="ikebukuro"; shozoku="丸ノ内線"};
+{kanji="新大塚"; kana="しんおおつか"; romaji="shinotsuka"; shozoku="丸ノ内線"};
+{kanji="茗荷谷"; kana="みょうがだに"; romaji="myogadani"; shozoku="丸ノ内線"};
+{kanji="後楽園"; kana="こうらくえん"; romaji="korakuen"; shozoku="丸ノ内線"};
+{kanji="本郷三丁目"; kana="ほんごうさんちょうめ"; romaji="hongosanchome"; shozoku="丸ノ内線"};
+{kanji="御茶ノ水"; kana="おちゃのみず"; romaji="ochanomizu"; shozoku="丸ノ内線"}
+]
+
+let test_make_initial_eki_list1 = make_initial_eki_list [] "茗荷谷" = []
+let test_make_initial_eki_list2 = make_initial_eki_list ekimei_list "茗荷谷" = [
+{namae="池袋"; saitan_kyori = infinity; temae_list = []};
+{namae="新大塚"; saitan_kyori = infinity; temae_list = []};
+{namae="茗荷谷"; saitan_kyori = 0.; temae_list = ["茗荷谷"]};
+{namae="後楽園"; saitan_kyori = infinity; temae_list = []};
+{namae="本郷三丁目"; saitan_kyori = infinity; temae_list = []};
+{namae="御茶ノ水"; saitan_kyori = infinity; temae_list = []}
+]
+
 let rec kana_insert lst input_ekimei_t = match lst with
   [] -> [input_ekimei_t]
   | first :: rest ->
