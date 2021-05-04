20330b09ee37876ab8927a96a2e91a95804716c6
yuki0920 <ywsep20@gmail.com>
2021-05-04 16:11:46 +0900

    sum_list.ml

--- sum_list.ml
+++ sum_list.ml
@@ -1,11 +1,9 @@
 (* sum_list int : lst -> int : list *)
 (* total は これまでの合計を保持しておく関数 *)
-let rec sum_list lst =
-  let rec total lst = match lst with
-    [] -> 0
-    | first :: rest -> first + total lst
-  in match lst with
+let sum_list lst =
+  let rec total lst accum = match lst with
     [] -> []
-    | first :: rest -> first + total :: rest
+    | first :: rest -> first + accum :: total rest (first + accum)
+  in total lst 0
 
 let test = sum_list [3; 2; 1; 4] = [3; 5; 6; 10]
